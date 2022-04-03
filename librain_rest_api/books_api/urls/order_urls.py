eda6c06109e0921f34982d04686367cb21abf9ce
Victoria Le <victoriale@Victorias-MacBook-Pro.local>
2022-04-03 11:29:16 -0400

    Frontend and backend hooked up, user able to retrieve order detail after placing order

--- librain_rest_api/books_api/urls/order_urls.py
+++ librain_rest_api/books_api/urls/order_urls.py
@@ -5,4 +5,5 @@ from books_api.views import order_views as views
 
 urlpatterns = [
     path('add/', views.addOrderItems, name='orders-add'),
+    path('<str:pk>/', views.getOrderById, name ='user-order'),
 ]
--- librain_rest_api/books_api/views/order_views.py
+++ librain_rest_api/books_api/views/order_views.py
@@ -53,3 +53,18 @@ def addOrderItems(request):
 
         serializer = OrderSerializer(order, many=False)
         return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+    user = request.user #this is from the token
+    try:
+        order = Order.objects.get(id=pk)
+        if user.is_staff or order.user == user :
+            serializer = OrderSerializer(order, many = False)
+            return Response(serializer.data)
+        else:
+            Response({'detail': 'Not authorized to view this order'}, status = status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail': 'Order is not existed'},status = status.HTTP_400_BAD_REQUEST )
